f21e491650c6aad69a7605635812ba31ebd7299f
Josef Fürlinger <josef.fuerlinger@smartpoint.at>
2020-02-28 22:06:43 +0100

    Zusätzliche LINQ-Implementierung (siehe Kurzquiz am 27.02.2020).

--- LiveCoding.ConsoleApp/Program.cs
+++ LiveCoding.ConsoleApp/Program.cs
@@ -9,11 +9,16 @@ namespace LiveCoding.ConsoleApp
 {
   class Program
   {
+    #region Constants
     const string FileNamePupils = "pupils.csv";
     const string FileNameClasses = "classes.csv";
+    #endregion
 
     static void Main(string[] args)
     {
+
+      DoSomeLinqStuff();
+
       IEnumerable<Pupil> pupils = LoadPupils();
       PrintResult("Schüler", pupils);
 
@@ -85,7 +90,7 @@ namespace LiveCoding.ConsoleApp
               };
             })
           .OrderByDescending(g => g.AnzSchueler)
-          );
+      );
 
       // Bilde die Summe aller ungerader Zahlen zw. 1 und 99
       int summe = Enumerable.Range(1, 99)
@@ -100,15 +105,57 @@ namespace LiveCoding.ConsoleApp
       Random rand = new Random();
       var randomNumbers = Enumerable.Range(1, 5)
         .Select(_ => rand.Next(1, 10 + 1));
-      
+
       PrintResult("1a. Zufallszahlen", randomNumbers);
       PrintResult("2a. Zufallszahlen", randomNumbers);
-     
+
       var persistedNumbers = randomNumbers.ToArray();
 
       PrintResult("1b. Zufallszahlen", persistedNumbers);
       PrintResult("2b. Zufallszahlen", persistedNumbers);
     }
+
+    private static void DoSomeLinqStuff()
+    {
+      //Ausgangssituation: eine C# Liste mit Abteilungsmitarbeitern (Name, Abteilung, Gehalt, etc.) 
+      List<Employee> employees = new List<Employee>()
+      {
+        new Employee() {FirstName = "Max", LastName = "Müller", Salary = 1500, Department = "Human Ressources"},
+        new Employee() {FirstName = "Lea", LastName = "Mustermann", Salary = 2500, Department = "Finance"},
+        new Employee() {FirstName = "Franz", LastName = "Schatzl", Salary = 1800, Department = "Operations"},
+        new Employee() {FirstName = "Heinz", LastName = "Huber", Salary = 1750, Department = "Operations"},
+        new Employee() {FirstName = "Simone", LastName = "Mair", Salary = 2400, Department = "Finance"},
+        new Employee() {FirstName = "Moritz", LastName = "Meier", Salary = 1800, Department = "Human Ressources"},
+        new Employee() {FirstName = "Tom", LastName = "Zweimüller", Salary = 3900, Department = "Operations"},
+        new Employee() {FirstName = "Angelika", LastName = "Goiserer", Salary = 1500, Department = "Finance"},
+        new Employee() {FirstName = "Toni", LastName = "Aufreiter", Salary = 3700, Department = "Services"},
+        new Employee() {FirstName = "Hubert", LastName = "Zauner", Salary = 4400, Department = "Finance"},
+        new Employee() {FirstName = "Wolfgang", LastName = "Hinterholzer", Salary = 1500, Department = "Services"},
+        new Employee() {FirstName = "Andrea", LastName = "Leeb", Salary = 2900, Department = "Services"},
+      };
+
+      // TODO: Gruppieren Sie nach der Abteilung und bilden Sie die Summe der Gehälter pro Abteilung
+      // TODO: Erstellen Sie einen anonymen Typ, welcher nur den Abteilungsnamen und die Gehaltssumme enthält
+      // TODO: Geben Sie die Abteilungen inkl. deren Gehälter absteigend nach deren Gesamtgehalt auf der Konsole aus.
+
+
+
+      var departmentsBySalary = employees
+        .MyWhere(e => e.Salary > 2000)
+        .GroupBy(e => e.Department)
+        .Select(g => new
+        {
+          Name = g.Key,
+          Sum = g.Sum(emp => emp.Salary)
+        })
+        .OrderByDescending(r => r.Sum);
+
+
+      var result = departmentsBySalary.Select(d => d.Name);
+
+      PrintResult("Abteilungen per Gehalt", result);
+    }
+
 
     /// <summary>
     /// Reads all the pupils from the csv-file.
@@ -151,18 +198,18 @@ namespace LiveCoding.ConsoleApp
     /// </summary>
     private static IEnumerable<Class> LoadClasses()
       => File.ReadAllLines(FileNameClasses)
-            .Skip(1)
-            .Select(line =>
-            {
-              string[] parts = line.Split(';');
+        .Skip(1)
+        .Select(line =>
+        {
+          string[] parts = line.Split(';');
 
-              return new Class()
-              {
-                Name = parts[0],
-                ClassType = (ClassType)Enum.Parse(typeof(ClassType), parts[1], true)
-              };
-            })
-            .OrderBy(c => c.Name);
+          return new Class()
+          {
+            Name = parts[0],
+            ClassType = (ClassType)Enum.Parse(typeof(ClassType), parts[1], true)
+          };
+        })
+        .OrderBy(c => c.Name);
 
 
     /// <summary>
@@ -191,6 +238,7 @@ namespace LiveCoding.ConsoleApp
       {
         Console.WriteLine(item.ToString());
       }
+
       Console.WriteLine();
     }
   }
---
+++ LiveCoding.Core/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace LiveCoding.Core
+{
+  public class Employee
+  {
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public double Salary { get; set; }
+    public string Department { get; set; }
+  }
+}
